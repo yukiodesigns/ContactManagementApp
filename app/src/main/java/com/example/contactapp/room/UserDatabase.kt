a0b67c3dd460805561e5786c3043edb97bc0fc73
Yukio <elsie.atieno@student.moringaschool.com>
2024-03-06 14:53:06 +0300

    Made Repository

--- app/src/main/java/com/example/contactapp/room/UserDatabase.kt
+++ app/src/main/java/com/example/contactapp/room/UserDatabase.kt
@@ -3,9 +3,10 @@ package com.example.contactapp.room
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Database(entities = [User::class], version = 1)
-abstract class UserDatabase {
+abstract class UserDatabase : RoomDatabase(){
     abstract val userDAO: UserDAO
 
     //Singleton
