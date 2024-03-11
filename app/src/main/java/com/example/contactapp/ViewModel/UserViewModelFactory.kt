49c0fd490477bb72a65d4534d79558b48d4eb2ba
Yukio <elsie.atieno@student.moringaschool.com>
2024-03-11 12:08:58 +0300

    Made view model Factory

---
+++ app/src/main/java/com/example/contactapp/ViewModel/UserViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.example.contactapp.ViewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.contactapp.room.UserRepo
+
+class UserViewModelFactory(private val repository: UserRepo): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(UserViewModel::class.java)){
+            return UserViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown View Model Class")
+    }
+}
