f7691af9a63cca4193237a3fc961fac56e735b31
Yukio <elsie.atieno@student.moringaschool.com>
2024-03-11 12:05:15 +0300

    Connect View Model with repo

--- app/src/main/java/com/example/contactapp/ViewModel/UserViewModel.kt
+++ app/src/main/java/com/example/contactapp/ViewModel/UserViewModel.kt
@@ -4,8 +4,10 @@ import androidx.databinding.Bindable
 import androidx.databinding.Observable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.contactapp.room.User
 import com.example.contactapp.room.UserRepo
+import kotlinx.coroutines.launch
 
 class UserViewModel(private val repository: UserRepo) : ViewModel(), Observable{
 
@@ -14,9 +16,53 @@ class UserViewModel(private val repository: UserRepo) : ViewModel(), Observable{
     private lateinit var userToUpdateOrDelete: User
 
     @Bindable
-    val inputName = MutableLiveData<String>()
+    val inputName = MutableLiveData<String?>()
 
     @Bindable
-    val inputEmail = MutableLiveData<String>()
+    val inputEmail = MutableLiveData<String?>()
+
+    @Bindable
+    val saveOrUpdateButtonText = MutableLiveData<String>()
+
+    @Bindable
+    val clearAllOrDeleteButtonText = MutableLiveData<String>()
+
+    init{
+        saveOrUpdateButtonText.value = "Save"
+        clearAllOrDeleteButtonText.value = "Clear All"
+
+    }
+
+    fun saveOrUpdate(){
+        val name = inputName.value !!
+        val email = inputEmail.value!!
+        insert(User(0,name, email))
+        inputName.value = null
+        inputEmail.value = null
+    }
+
+    fun clearAllOrDelete(){
+        clearAll()
+    }
+    fun insert(user:User) = viewModelScope.launch {
+        repository.insert((user))
+    }
+    fun clearAll() = viewModelScope.launch {
+        repository.deleteAll()
+    }
+    fun update(user:User) = viewModelScope.launch {
+        repository.update(user)
+    }
+    fun delete(user:User) = viewModelScope.launch {
+        repository.delete(user)
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+
+    }
 
 }
